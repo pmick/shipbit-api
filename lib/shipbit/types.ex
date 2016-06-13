f1b1f16d348d5792b56cbfd361ea099446443a53
pmick <patrickmick1@gmail.com>
2016-06-12 21:39:56 -0700

    Adding a game type

---
+++ lib/shipbit/types.ex
@@ -0,0 +1,7 @@
+defmodule Next.Types do
+  defmodule Game do
+    defstruct [:id, :art_path, :critic_score, :developer, :esrb,
+               :genre, :link_path, :platforms, :publisher,
+               :release_date, :summary, :title]
+  end
+end
