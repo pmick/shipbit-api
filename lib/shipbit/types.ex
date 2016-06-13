a62b42d44b95f678e1b17033181f9c305730984e
pmick <patrickmick1@gmail.com>
2016-06-12 23:12:14 -0700

    Updating types namespace

--- lib/shipbit/types.ex
+++ lib/shipbit/types.ex
@@ -1,4 +1,4 @@
-defmodule Next.Types do
+defmodule Shipbit.Types do
   defmodule Game do
     defstruct [:id, :art_path, :critic_score, :developer, :esrb,
                :genre, :link_path, :platforms, :publisher,
